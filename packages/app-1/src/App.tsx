2795c1288b2a2a6266d5241b5fdb66a2c4ade457
akashi <akashi_sai@163.com>
2022-08-28 14:20:01 +0800

    :tada: chore: update deps

--- packages/app-1/src/App.tsx
+++ packages/app-1/src/App.tsx
@@ -3,8 +3,6 @@ import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
       <div>
@@ -16,17 +14,6 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
   )
 }
